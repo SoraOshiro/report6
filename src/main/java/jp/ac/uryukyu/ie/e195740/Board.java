1811ffb89294f9b9e6d41c8138452509d02d53e4
SoraOoshiro <e195740@ie.u-ryukyu.ac.jp>
2020-01-20 23:13:01 +0900

    pass機能実装

--- src/main/java/jp/ac/uryukyu/ie/e195740/Board.java
+++ src/main/java/jp/ac/uryukyu/ie/e195740/Board.java
@@ -1,4 +1,5 @@
 package jp.ac.uryukyu.ie.e195740;
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Board {
@@ -11,22 +12,45 @@ public class Board {
     void setDisc(Disc disc){
         boolean checkBoard = false;
         System.out.println("Now "+disc.getPlayerName()+"'s turn.");
-        while(checkBoard == false){
-            System.out.println("plz insert rows num.(1~8)");
-            Scanner row = new Scanner(System.in);
-            int rNum = row.nextInt();
-            System.out.println("plz insert columns num.(1~8)");
-            Scanner column = new Scanner(System.in);
-            int cNum = column.nextInt();
-            if(rNum>8||rNum<1||cNum>8||cNum<1||board[rNum-1][cNum-1] != null){
-                System.out.println("cannot action. plz reinsert.");
-            }else{
-                board[rNum-1][cNum-1] = disc.getDiscColour();
-                disc.count();
-                checkBoard = true;
+        boolean pass = passCheck();
+        if(!pass) {
+            while (!checkBoard) {
+                System.out.println("plz insert rows num.(1~8)");
+                Scanner row = new Scanner(System.in);
+                int rNum = row.nextInt();
+                System.out.println("plz insert columns num.(1~8)");
+                Scanner column = new Scanner(System.in);
+                int cNum = column.nextInt();
+                if (rNum > 8 || rNum < 1 || cNum > 8 || cNum < 1 || board[rNum - 1][cNum - 1] != null) {
+                    System.out.println("cannot action. plz reinsert.");
+                } else {
+                    board[rNum - 1][cNum - 1] = disc.getDiscColour();
+                    disc.count();
+                    checkBoard = true;
+                }
             }
         }
+    }
 
+    boolean passCheck(){
+        boolean pCheck = false;
+        boolean returnCheck = false;
+        String pass;
+        System.out.println("Do you wanna pass?(y/n)");
+        while(!pCheck){
+            Scanner pc = new Scanner(System.in);
+            pass = pc.next();
+            if(pass.equals("y")){
+                pCheck = true;;
+                returnCheck = true;
+            }else if(pass.equals("n")){
+                pCheck = true;
+                returnCheck = false;
+            }else{
+                System.out.println("(y/n)");
+            }
+        }
+        return returnCheck;
     }
 
     //getter
